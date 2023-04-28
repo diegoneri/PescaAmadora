8c2cd4f5f4ef26c96096e830afdc9ea4a13f7140
Diego Neri <diego.neri@etec.sp.gov.br>
2023-04-28 20:33:44 -0300

    Finaliza programa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string AGUAS_MARINHAS = "M";
+const string AGUAS_CONTINENTAIS = "C";
+
+const double LimiteAguasContinentaisEmKg = 10;
+const double LimiteAguasMarinhasEmKg = 15;
+
+const decimal MultaExcessoPeso = 1000;
+const decimal MultaPorKgExcedido = 20;
+
+double PesoDoPescadoEmKg;
+string LocalPesca;
+
+Console.Clear();
+
+Console.WriteLine("--- Pesca Amadora ---\n");
+
+Console.Write("Peso (em kg)...: ");
+
+PesoDoPescadoEmKg = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Águas [c]ontinentais ou [m]arinhas? ");
+LocalPesca = Console.ReadLine()!
+                .Trim()
+                .Substring(0, 1)
+                .ToUpper();
+
+Console.WriteLine();
+
+if (LocalPesca != AGUAS_CONTINENTAIS 
+ && LocalPesca != AGUAS_MARINHAS)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Local não reconhecido.");
+    Console.ResetColor();
+    return;
+}
+
+if ((LocalPesca == AGUAS_CONTINENTAIS 
+    && PesoDoPescadoEmKg <= LimiteAguasContinentaisEmKg) 
+    ||
+    (LocalPesca == AGUAS_MARINHAS
+    && PesoDoPescadoEmKg <= LimiteAguasMarinhasEmKg))
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("Pescaria dentro dos limites legais.");
+    Console.ResetColor();
+    return;       
+}
+
+double PesoPermitido;
+
+if (LocalPesca == AGUAS_CONTINENTAIS)
+{
+    PesoPermitido = LimiteAguasContinentaisEmKg;
+}
+else
+{
+    PesoPermitido = LimiteAguasMarinhasEmKg;
+}
+
+double PesoEmExcesso = PesoDoPescadoEmKg - PesoPermitido;
+
+decimal Multa = MultaExcessoPeso + 
+                 (MultaPorKgExcedido * Convert.ToDecimal(PesoEmExcesso));
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+Console.WriteLine($"Pescaria excede os limites legais em {PesoEmExcesso}kg.");
+Console.WriteLine($"Sujeito a multa de {Multa:C2}.");
+
+Console.ResetColor();
